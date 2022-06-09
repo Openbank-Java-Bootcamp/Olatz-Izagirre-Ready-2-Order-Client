0430c3576dfc4fc6ca1eedd2362f804ce3b78805
Olatz Izagirre Iturregi <olatzizagirre@hotmail.com>
2022-06-09 14:48:30 +0200

    Thursday morning

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+import IsAnon from "./components/IsAnon";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/signup"
+          element={
+            <IsAnon>
+              <SignupPage />
+            </IsAnon>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <IsAnon>
+              <LoginPage />
+            </IsAnon>
+          }
+        />
+      </Routes>
     </div>
   );
 }
